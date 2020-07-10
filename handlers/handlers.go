66c5ade3975629f783a25dc629c7331c2cb98e83
h3isenbug <h.kalantari.1997@gmail.com>
2020-07-10 21:17:35 +0430

    added SendError function

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,9 +2,15 @@ package handlers
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"net/http"
 )
+
+type Response struct {
+	Message string      `json:"message,omitempty"`
+	Data    interface{} `json:"data,omitempty"`
+}
 
 func GetURLParams(r *http.Request) map[string]string {
 	value := r.Context().Value("params")
@@ -15,10 +21,21 @@ func GetURLParams(r *http.Request) map[string]string {
 	return value.(map[string]string)
 }
 
-
 func GorillaMuxURLParamMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := context.WithValue(r.Context(), "params", mux.Vars(r))
 		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func SendError(w http.ResponseWriter, statusCode int) error {
+	return SendErrorWithCustomMessage(w, statusCode, http.StatusText(statusCode))
+}
+
+func SendErrorWithCustomMessage(w http.ResponseWriter, statusCode int, message string) error {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	return json.NewEncoder(w).Encode(&Response{
+		Message: message,
 	})
 }
